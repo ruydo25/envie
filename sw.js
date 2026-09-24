3a8d74aa7cc3350e622e9fa4597d01f7085171d4
ruydo25 <ruydo25@gmail.com>
2026-09-24 11:05:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'envie-v1';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    fetch(req).then(res => {
+      try {
+        if (res && res.status === 200 && new URL(req.url).origin === self.location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(req, copy));
+        }
+      } catch (err) {}
+      return res;
+    }).catch(() => caches.match(req))
+  );
+});
